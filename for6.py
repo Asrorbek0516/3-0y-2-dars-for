2c43cdfe7b290c7d42a2027953756986a6eb9080
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-06-30 13:50:34 +0500

    for6 misol

--- for6.py
+++ for6.py
@@ -1,4 +1,5 @@
 narx = float(input("1_kg konfet narxi: "))
 
-for i in range(1.2,2.1,0.2):
-    print(f"{i*0.1:.1f} kg konfet narxi {i*0.1*narx:.2f} dollar")
+for i in range(12,21,2):
+    kg = i/10
+    print(f"{kg:.1f} kg konfet narxi {kg*narx:.2f} dollar")
